3e245dfd3e50e1c6fd2edb3ecb91b89e5474ed44
mox93 <mohamed.ragaiy.saleh@gmail.com>
2020-09-19 17:25:27 +0200

    Compiled the code

---
+++ build.sh
@@ -0,0 +1,6 @@
+
+elm make src/Main.elm --output ./index/elm.js --debug
+
+#elm make src/Main.elm --output elm.js --optimize
+
+#./optimize.sh src/Main.elm
---
+++ index/main.js
@@ -0,0 +1,4 @@
+console.log("Starting...");
+
+// init elm app
+var app = Elm.Main.init();
